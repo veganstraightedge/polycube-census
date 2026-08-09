3a4d57ddc5f7780bb11cdf40d91fbc3f3b323454
Shane Becker <veganstraightedge@gmail.com>
2026-08-08 19:41:42 -0700

    Remove debug output from Pipeline

--- lib/census/pipeline.rb
+++ lib/census/pipeline.rb
@@ -27,8 +27,6 @@ module Census
     end
 
     def process(path, &report)
-      puts path
-      puts
       record = JSON.parse(File.read(path), symbolize_names: true)
       return if record[:verdict]
 
